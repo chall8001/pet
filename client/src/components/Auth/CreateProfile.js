b283a9b8197309d8498ff705fc87cd19e4d714cb
Charles Hall <chall8001@gmail.com>
2021-03-30 12:54:06 -0600

    fixed merging conflicts

--- client/src/components/Auth/CreateProfile.js
+++ client/src/components/Auth/CreateProfile.js
@@ -1,33 +1,61 @@
-import React from 'react';
-import { useForm } from "react-hook-form"
+import React from "react";
+import { useForm } from "react-hook-form";
 
-function CreateUser(){
-const {register, handleSubmit} = useForm()
+function CreateUser() {
+  const { register, handleSubmit } = useForm();
 
-const onSubmit = (data) => {
-    console.log(data)
-    fetch('/create-profile', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(data),
-})
-}
+  const onSubmit = (data) => {
+    console.log(data);
+    fetch("/create-profile", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  };
 
-return(
+  return (
     <form onSubmit={handleSubmit(onSubmit)}>
-    <input type="password" placeholder="Pet Name" name="petName" ref={register({required: true})}/>
-    <input type="data" placeholder="Your Name" name="memberName" ref={register({required: true})}/>
-    <input type="data" placeholder="Location" name="location" ref={register({required: true})}/>
-    <input type="data" placeholder="Breed" name="breedSize" ref={register({required: true})}/>
-    <input type="data" placeholder="Birth-Day" name="birthDay" ref={register({required: true})}/>
-    <input type="data" placeholder="Favorite Activities" name="favoriteActivities" ref={register({required: true})}/>
-    <input type="submit" />
+      <input
+        type="password"
+        placeholder="Pet Name"
+        name="petName"
+        ref={register({ required: true })}
+      />
+      <input
+        type="data"
+        placeholder="Your Name"
+        name="ownerName"
+        ref={register({ required: true })}
+      />
+      <input
+        type="data"
+        placeholder="Location"
+        name="location"
+        ref={register({ required: true })}
+      />
+      <input
+        type="data"
+        placeholder="Pet Type"
+        name="petType"
+        ref={register({ required: true })}
+      />
+      <input
+        type="data"
+        placeholder="Birth-Day"
+        name="birthDay"
+        ref={register({ required: true })}
+      />
+      <input
+        type="data"
+        placeholder="Favorite Activities"
+        name="favoriteActivities"
+        ref={register({ required: true })}
+      />
+      <input type="submit" />
     </form>
-)
-
-
+  );
 }
 
 export default CreateUser;
---
+++ models/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  Owner: require("./owner"),
+};
