2a5a481fcf5351f690faaa04fdb24f92f3bbeda6
Blair Taylor <blairntaylor@gmail.com>
2021-03-31 13:11:21 -0600

    complete card for profile page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,35 +1,35 @@
 import React from "react";
-import { Image } from 'semantic-ui-react'
+import { Image } from "semantic-ui-react";
 import "../App.css";
 import { Link } from "react-router-dom";
 import LoginBtn from "./Auth/LoginBtn";
 import LogoutBtn from "./Auth/LogoutBtn";
-import './Navbar.css'
+import "./Navbar.css";
 
 function Navbar() {
   return (
     <div>
-    <LoginBtn />
-    <LogoutBtn />
-    <nav className="navbar">
-       <div className="navbar-container">
-       <Image src='./images/logo.png'  size='small' rounded />
-      <ul className="list">
-        <Link to="/">
-          <li> Homepage </li>
-        </Link>
-        <Link to="/Favorite">
-          <li> Favorites </li>
-        </Link>
-        <Link to="/userprofile">
-          <li> My Profile </li>
-        </Link>
-        <Link to="/chatroom">
-          <li> Chat Room  </li>
-        </Link>
-      </ul>
-      </div>
-    </nav>
+      <LoginBtn />
+      <LogoutBtn />
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Image src="./images/logo.png" size="small" rounded />
+          <ul className="list">
+            <Link to="/">
+              <li> Homepage </li>
+            </Link>
+            <Link to="/Favorite">
+              <li> Favorites </li>
+            </Link>
+            <Link to="/Yourprofile">
+              <li> My Profile </li>
+            </Link>
+            <Link to="/chatroom">
+              <li> Chat Room </li>
+            </Link>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
---
+++ client/src/components/dynamCard.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+function dynamCard(
+  image,
+  petName,
+  memberName,
+  location,
+  breedSize,
+  birthday,
+  favorites
+) {
+  return (
+    <div>
+      <Card>
+        <Image src={image} alt="pet-image" />
+        <Card.Content>
+          <Card.Header>
+            <h1>PetName</h1>
+            {petName}
+          </Card.Header>
+          <Card.Header>
+            <h2>MemberName</h2>
+            {memberName}
+          </Card.Header>
+          <Card.Meta>City, State{location}</Card.Meta>
+          <Card.Meta>BreedSize{breedSize}</Card.Meta>
+          <Card.Meta>Birthday{birthday}</Card.Meta>
+          <Card.Meta>FavoriteActivities{favorites}</Card.Meta>
+        </Card.Content>
+        <Card.Content>
+          <Button color="blue">Edit Profile</Button>
+        </Card.Content>
+      </Card>
+    </div>
+  );
+}
+
+export default dynamCard;
