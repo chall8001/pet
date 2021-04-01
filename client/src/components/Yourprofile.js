1f9a8607dd9021b73bd03011d6fc01700899df6f
Drew Weston <awfiscus@comcast.net>
2021-04-01 14:42:31 -0600

    profile card is not filled out dynamically based on the user info put in through the profile input and the user logged in through the auth0

--- client/src/components/Yourprofile.js
+++ client/src/components/Yourprofile.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Card } from "semantic-ui-react";
 import "./Yourprofile.css";
 import Image from "./images/3.jpg";
+import API from "../utils/API";
 
 function Yourprofile() {
+  const [owner, setOwner] = useState({});
   const { user, isAuthenticated, isLoading } = useAuth0();
+
+  console.log(user);
+  useEffect(() => {
+    loadOwnerProfile();
+  }, []);
+
+  function formatDate(date) {
+    var d = new Date(date),
+      month = "" + (d.getMonth() + 1),
+      day = "" + d.getDate(),
+      year = d.getFullYear();
+
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
+
+    return [month, day, year].join("-");
+  }
+
+  function loadOwnerProfile() {
+    API.getOwner(user.email)
+      .then((res) => {
+        console.log(res);
+        setOwner(res.data);
+      })
+      .then(console.log(owner))
+      .catch((err) => console.log(err));
+  }
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -20,11 +49,11 @@ function Yourprofile() {
       <div className="card-container">
         <Card
           image={Image}
-          header="Pet Name: Lulu"
-          meta="Username: Andrew"
-          description="Location: Denver CO"
-          description="Hobbies: Running, playing, laying in the sun."
-          extra="Birthday: Feb 13 2005"
+          header={`Pet Name: ${owner.petName}`}
+          meta={`Username: ${owner.name}`}
+          description={`Location: ${owner.location}`}
+          description={`Hobbies: ${owner.hobbies}`}
+          extra={`Birthday: ${formatDate(owner.birthDay)}`}
         />
       </div>
     )
