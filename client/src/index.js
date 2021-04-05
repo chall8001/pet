949683114633b7a11024d9270f27ef02cd3e9aa6
Drew Weston <awfiscus@comcast.net>
2021-04-05 15:09:32 -0600

    added code to auth to have it work on heroku

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ ReactDOM.render(
   <Auth0Provider
     domain="dev-h34u236j.us.auth0.com"
     clientId="DzRX0bSwb4PHukU6hPoo9Mc9gsvU8xxH"
-    redirectUri="http://localhost:3000/Yourprofile"
+    redirectUri={window.location.origin}
   >
     <App />
   </Auth0Provider>,
